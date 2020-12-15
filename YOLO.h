12d1f9c7ae12dee58b2d1ed7bdab8b920e3eba1f
DoJing <lydujing@163.com>
2020-12-15 11:08:51 +0800

    适配opencv低版本2

--- YOLO.h
+++ YOLO.h
@@ -13,7 +13,7 @@
 
 #include <opencv2/opencv.hpp>
 #include <opencv2/core/core.hpp>
-#include <opencv2/dnn.hpp>
+#include "dnn/dnn.hpp"
 #include <opencv2/imgproc.hpp>
 #include <opencv2/highgui.hpp>
 
